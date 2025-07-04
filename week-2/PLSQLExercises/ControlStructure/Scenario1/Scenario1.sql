d6a866e507c5c0d2f8d2a51f56470853ed4f7d7f
sambath004 <2022pecit305@gmail.com>
2025-07-04 18:52:36 +0530

    Update Scenario1.sql

--- week-2/PLSQLExercises/ControlStructure/Scenario1/Scenario1.sql
+++ week-2/PLSQLExercises/ControlStructure/Scenario1/Scenario1.sql
@@ -1,1 +1,11 @@
+begin
+  for cust in (select customer_id, interest_rate, age from customers) loop
+    if cust.age > 60 then
+      update customers
+      set interest_rate = interest_rate - 1
+      where customer_id = cust.customer_id;
+    end if;
+  end loop;
+end;
+/
 SELECT * FROM savings_accounts;
